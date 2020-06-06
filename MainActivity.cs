a74a0464de9613a5a86d4aaadc11e515fada10e6
Cyril De Brito <developpeur@cyrildebrito.info>
2020-06-06 18:19:31 +0200

    Update front with 3 btn

--- MainActivity.cs
+++ MainActivity.cs
@@ -13,17 +13,39 @@ namespace XamarinAppAndroidIOT
     public class MainActivity : AppCompatActivity
     {
 
+        Button BtnVoice1;
+        Button BtnVoice2;
+        Button BtnVoice3;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
 
-            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
-            SetSupportActionBar(toolbar);
+            BtnVoice1 = FindViewById<Button>(Resource.Id.btnVoice1);   
+            BtnVoice2 = FindViewById<Button>(Resource.Id.btnVoice2);
+            BtnVoice3 = FindViewById<Button>(Resource.Id.btnVoice3);
 
-            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
-            fab.Click += FabOnClick;
+            BtnVoice1.Click += BtnVoice1_Click;
+            BtnVoice2.Click += BtnVoice2_Click;
+            BtnVoice3.Click += BtnVoice3_Click;
+
+        }
+
+        private void BtnVoice1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void BtnVoice2_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void BtnVoice3_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
 
         public override bool OnCreateOptionsMenu(IMenu menu)
